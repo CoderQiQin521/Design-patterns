c5c6a5177347b5755e461b3df0caf11f1fc04fa5
CoderQiQin521 <coderqiqin@aliyun.com>
2020-09-11 21:33:06 +0800

    基于javascript-state-machine实现状态模式

--- src/List/ListItem.js
+++ src/List/ListItem.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import { getCart } from '../Shoppingcart/getCart';
+import StateMachine from 'javascript-state-machine';
 // import { log } from '../utils/log';
 
 export default class ListItem {
@@ -25,14 +26,47 @@ export default class ListItem {
 
     // @log('add')
     _initBtn() {
+        const $btn = $('<button>');
         let that = this;
-        let el = this.el;
-        const btn = $('<button>添加到购物车</button>');
-        btn.click(function () {
-            // TODO: 状态机
-            that._addToCartHandle();
+
+        //  状态模式
+        let fsm = new StateMachine({
+            init: '添加到购物车',
+            transitions: [
+                {
+                    name: 'addToCart',
+                    from: '添加到购物车',
+                    to: '移出购物车',
+                },
+                {
+                    name: 'deleteFromCart',
+                    from: '移出购物车',
+                    to: '添加到购物车',
+                },
+            ],
+            methods: {
+                onAddToCart() {
+                    that._addToCartHandle();
+                    updateText();
+                },
+                onDeleteFromCart() {
+                    that._deleteFromCartHandle();
+                    updateText();
+                },
+            },
         });
-        el.append(btn);
+
+        function updateText() {
+            $btn.text(fsm.state);
+        }
+
+        $btn.click(function () {
+            // 状态机
+            fsm.is('添加到购物车') ? fsm.addToCart() : fsm.deleteFromCart();
+        });
+
+        updateText();
+        this.el.append($btn);
     }
     _addToCartHandle() {
         this.cart.add(this.data);
